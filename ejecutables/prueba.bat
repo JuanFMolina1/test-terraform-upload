37c109567e72d09e881081e6a37a4158a2389588
JuanFMolina1 <juanchofelipe1128@gmail.com>
2022-05-16 14:22:35 -0500

    se elimina carpeta img

--- ejecutables/prueba.bat
+++ ejecutables/prueba.bat
@@ -4,7 +4,11 @@ set url_storage=https://terraformteststoracc.blob.core.windows.net/
 set container_name=web
 set user=soporte@audisoft.com
 set password=8rSYHVBGLQIoymHQb9um+
-set source=C:\Users\soporte\Documents\AudiSoft\repos\test-terraform-upload\
+set source=C:\Users\soporte\Documents\AudiSoft\repos\test-terraform-upload\pagina_web
+set path=C:\Users\soporte\Documents\AudiSoft\repos\test-terraform-upload
+
+cd %path%
+git pull
 
 echo "Loggin In... %user%"
 call az login -u %user% -p %password%
